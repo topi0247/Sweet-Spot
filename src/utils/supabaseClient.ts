ac141e6a16d006a849ee61cc51a593fc88616da8
KanaeNishina <karubona.ra247@gmail.com>
2024-01-29 11:01:43 +0900

    デプロイエラー確認

--- src/utils/supabaseClient.ts
+++ src/utils/supabaseClient.ts
@@ -9,6 +9,9 @@ const SUPABASE_ANON_KEY =
     ? process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
     : process.env.SUPABASE_ANON_KEY! ?? "";
 
+console.log(SUPABASE_URL);
+console.log(SUPABASE_ANON_KEY);
+
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: { persistSession: false },
 });

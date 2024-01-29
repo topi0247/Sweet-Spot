48ca769cca29137dc034c1b1a603e9dee40056fc
KanaeNishina <karubona.ra247@gmail.com>
2024-01-29 11:03:29 +0900

    デプロイエラー確認

--- src/utils/supabaseClient.ts
+++ src/utils/supabaseClient.ts
@@ -1,16 +1,16 @@
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL =
-  process.env.NODE_ENV === "development"
-    ? process.env.NEXT_PUBLIC_SUPABASE_URL!
-    : process.env.SUPABASE_URL! ?? "";
-const SUPABASE_ANON_KEY =
-  process.env.NODE_ENV === "development"
-    ? process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-    : process.env.SUPABASE_ANON_KEY! ?? "";
+// const SUPABASE_URL =
+//   process.env.NODE_ENV === "development"
+//     ? process.env.NEXT_PUBLIC_SUPABASE_URL!
+//     : process.env.SUPABASE_URL! ?? "";
+// const SUPABASE_ANON_KEY =
+//   process.env.NODE_ENV === "development"
+//     ? process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+//     : process.env.SUPABASE_ANON_KEY! ?? "";
 
-console.log(SUPABASE_URL);
-console.log(SUPABASE_ANON_KEY);
+const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: { persistSession: false },
